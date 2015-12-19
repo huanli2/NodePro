32b95719c12d07f0d0aba6ad77d9fefed39e42a0
huanli2 <nier_123@yeah.net>
2015-12-19 16:44:13 +0800

    implment get token

--- DockerTest/index.js
+++ DockerTest/index.js
@@ -3,15 +3,27 @@
  */
 
 var express = require('express');
+var jwt = require('jsonwebtoken');
 
 var PORT = 9002;
 
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('hello world\n');
+var secrete;
+
+(function init() {
+    var args = process.argv;
+    secrete = args[2];
+})();
+
+function setToken(tokenObj, secret) {
+    return jwt.sign(tokenObj, secret);
+}
+
+app.get('/token', function (req, res) {
+    res.send(res, setToken(req.query, secrete));
 });
 
 app.listen(PORT);
 
-console.log("Running on httP://localhost:" + PORT);
+console.log("Running on http://localhost:" + PORT);
