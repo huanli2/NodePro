57c146cbc28bd16fbc182f3e38c4ee628c10cf1a
huanli2 <nier_123@yeah.net>
2016-05-18 15:28:10 +0800

    add router print for get token

--- DockerTest/index.js
+++ DockerTest/index.js
@@ -4,25 +4,38 @@
 
 var express = require('express');
 var jwt = require('jsonwebtoken');
+var app = express();
 
 var PORT = 9002;
 
-var app = express();
-
 var secrete;
 
-(function init() {
-    var args = process.argv;
-    secrete = args[2];
-})();
+app.use(function (req, res, next) {
+    console.log('Hello world!')
+    next()
+})
 
-function setToken(tokenObj, secret) {
-    return jwt.sign(tokenObj, secret);
-}
+app.use(function __helloWorld(req, res, next) { // <-- '__helloWorld' will be printed now 
+    next()
+})
 
 app.get('/token', function (req, res) {
     res.send(res, setToken(req.query, secrete));
 });
+
+(function init() {
+    var args = process.argv;
+    secrete = args[2];
+	
+	var path = require('path');
+    var filepath = path.join(__dirname, './docs/routes.gernerated.txt');
+    var print = require('express-print-routes')(app, filepath);
+})();
+
+
+function setToken(tokenObj, secret) {
+    return jwt.sign(tokenObj, secret);
+}
 
 app.listen(PORT);
 
